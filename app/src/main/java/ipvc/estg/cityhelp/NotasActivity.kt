5b49b7d30a8b67b94da6aa13b1ea66a7473fddfd
pedromcsousa <pedrosousa1@sapo.pt>
2021-03-05 20:32:36 +0000

    Instalação de dependência SweetAlertDialog

--- app/src/main/java/ipvc/estg/cityhelp/NotasActivity.kt
+++ app/src/main/java/ipvc/estg/cityhelp/NotasActivity.kt
@@ -2,11 +2,16 @@ package ipvc.estg.cityhelp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import cn.pedant.SweetAlert.SweetAlertDialog;
 
 class NotasActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notas)
+
+        SweetAlertDialog(this@NotasActivity)
+            .setTitleText("Alert Title")
+            .show()
     }
 }
