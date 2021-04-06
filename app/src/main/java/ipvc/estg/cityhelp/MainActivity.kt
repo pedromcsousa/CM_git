c601350af4dd64903d782456895f54992b3f0db0
pedromcsousa <pedrosousa1@sapo.pt>
2021-04-06 18:37:26 +0100

    Visualização de mapa estático

--- app/src/main/java/ipvc/estg/cityhelp/MainActivity.kt
+++ app/src/main/java/ipvc/estg/cityhelp/MainActivity.kt
@@ -5,16 +5,12 @@ import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import ipvc.estg.cityhelp.api.EndPoints
-import ipvc.estg.cityhelp.api.OutputGeral
-import ipvc.estg.cityhelp.api.ServiceBuilder
 
 class MainActivity : AppCompatActivity() {
 
@@ -32,6 +28,7 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
             )
         )
+
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
