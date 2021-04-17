df6763b5efb3daa86b72ea2466d73a629fd788f3
pedromcsousa <pedrosousa1@sapo.pt>
2021-04-17 14:35:17 +0100

    Spinner: alterações

--- app/src/main/java/ipvc/estg/cityhelp/ui/add/AddFragment.kt
+++ app/src/main/java/ipvc/estg/cityhelp/ui/add/AddFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Spinner
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -26,6 +29,30 @@ class AddFragment : Fragment() {
         dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+
+        //ELEMENTOS GERAIS
+
+        //ELEMENTOS FORMULÁRIO
+        val titulo : TextView = root.findViewById(R.id.textTituloSituacao)
+        val tipo : Spinner = root.findViewById(R.id.spinnerTipoSituacao)
+        val conteudo : TextView = root.findViewById(R.id.textDescSituacao)
+
+        //SPINNER - CARREGAR TIPOS
+        val spinner: Spinner = root.findViewById(R.id.spinnerTipoSituacao)
+        ArrayAdapter.createFromResource(
+            this.requireContext(),
+            R.array.tiposSituacao,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+
+        val btnAddSituacao : Button = root.findViewById(R.id.btnAddSituacao)
+        btnAddSituacao.setOnClickListener{
+            println(titulo.text.toString() + " - " + tipo.selectedItem + " (" + tipo.selectedItemId + ") - " + conteudo.text.toString())
+        }
+
         return root
     }
 }
